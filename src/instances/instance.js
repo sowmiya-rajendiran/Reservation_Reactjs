8c35ebedd6d8121bf344fdfcef81b249c47ad97d
Sowmiya Rajendiran <sowmiraj910@gmail.com>
2025-09-05 13:29:32 +0530

    updated

--- src/instances/instance.js
+++ src/instances/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = `https://reservation-nodejs.onrender.com`;
-// const baseURL = `http://localhost:3005/api`;
+// const baseURL = `http://localhost:3005`;
 
 let Instance = axios.create({
     baseURL,
@@ -12,5 +12,16 @@ let Instance = axios.create({
     },
     timeout: 5000 
 })
+
+Instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default Instance;
